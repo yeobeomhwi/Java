4565d60c0630fa223f7b458512668a80a421f7a2
yeobeomhwi <yeobh0828@naver.com>
2022-11-22 15:59:56 +0900

    Update Main.java

--- AimTest/src/AimTest/Main.java
+++ AimTest/src/AimTest/Main.java
@@ -6,7 +6,6 @@ public class Main {
   public static int SCREEN_HEIGHT = 1080; // 전체 높이 public static은 모든 프로젝트 내부에서 공유할수 있는것
 
   public static void main(String[] args) {
-    // TODO 자동 생성된 메소드 스텁
     new AimTest();
 
   }
