321e8dc75bc2399027e1ca5657147dd3c800daa1
yeobeomhwi <yeobh0828@naver.com>
2022-11-29 14:28:30 +0900

    Update Main.java

--- AimTest/src/AimTest/Main.java
+++ AimTest/src/AimTest/Main.java
@@ -2,8 +2,8 @@ package AimTest;
 
 public class Main {
 
-  public static int SCREEN_WIDTH = 1920; // 전체 넓이 final은 절대 바뀌지않는것
-  public static int SCREEN_HEIGHT = 1080; // 전체 높이 public static은 모든 프로젝트 내부에서 공유할수 있는것
+  public final static int SCREEN_WIDTH = 1920; // 전체 넓이 final은 절대 바뀌지않는것
+  public final static int SCREEN_HEIGHT = 1080; // 전체 높이 public static은 모든 프로젝트 내부에서 공유할수 있는것
 
   public static void main(String[] args) {
     new AimTest();
